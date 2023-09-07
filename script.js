f3639abe7cc8d0b1d28fbf26cf826b5de0d72997
hojinchang <hojinchang1998@gmail.com>
2023-09-07 15:48:35 -0700

    Add variable grid
    The grid box sizes are now adjustable.
    The grid size is adjusted using the slider input.

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 const gridContainer = document.querySelector(".grid-container");
-
-
 const sliderInput = document.querySelector(".slider");
 const sliderText = document.querySelector(".slider-text");
 
+function parseGrid (gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        const row = gridContainer.appendChild(document.createElement("div"));
+        for (let j = 0; j < gridSize; j++) {
+            const square = document.createElement("div");
+            square.className = "square";
+            row.appendChild(square);
+        }
+    }
+}
+
+function adjustSquareSize(gridWidth, gridSize) {
+    const squares = document.querySelectorAll(".square");
+    const squareSize = gridWidth / gridSize;
+
+    squares.forEach(square => {
+        square.style.cssText = `width: ${squareSize}px; height: ${squareSize}px;`
+    })
+}
+
+function createGrid(gridWidth, gridSize) {
+    parseGrid(gridSize);
+    adjustSquareSize(gridWidth, gridSize);
+}
+
+function adjustGrid(gridWidth, gridSize) {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    sliderText.textContent = `${gridSize} x ${gridSize}`;
+    createGrid(gridWidth, gridSize);
+}
+
+
+
+
+
+let gridSize = sliderInput.value;
+const gridWidth = parseInt(getComputedStyle(gridContainer).width, 10);
+createGrid(gridWidth, gridSize);
+
 
 sliderInput.addEventListener("input", function() {
-    const gridSize = sliderInput.value
-    sliderText.textContent = `${gridSize} x ${gridSize}`;
-
+    gridSize = sliderInput.value;
+    adjustGrid(gridWidth, gridSize);
 });
-console.log(sliderInput.value);
